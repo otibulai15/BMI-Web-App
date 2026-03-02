924feb9bdcd17a3f8d92cc5edb37c447e3d2012b
Kay Grewe <kg@iniationware.com>
2026-03-02 10:46:31 +0100

    fix: localstorage

--- src/line_chart/line_chart.js
+++ src/line_chart/line_chart.js
@@ -1,36 +1,10 @@
 /**
- *
- * @param {string} url path to file
- * @param {*} cb callback to handle the success or error case!
- */
-function loadJson(url, cb) {
-  const xhr = new XMLHttpRequest();
-  xhr.open("GET", url, true);
-  xhr.responseType = "json";
-
-  xhr.onload = () => {
-    if (xhr.status === 200) {
-      cb(null, xhr.response);
-    } else {
-      cb(new Error(`HTTP ${xhr.status}`));
-    }
-  };
-
-  xhr.onerror = () => cb(new Error("Network error while loading JSON"));
-
-  xhr.send();
-}
-
-/**
- *
  * @param {string} chartAnchorPoint A id to a canvas element to which the line diagram will be drawn
  * @returns a new Chart.js plot which contains the plotted data
  */
 function plotChart(chartAnchorPoint) {
   const chartAnchor = document.getElementById(chartAnchorPoint);
-  const plotData = [];
-  const rawPlotData = JSON.parse(localStorage.getItem("bmiData"));
-  plotData.push(rawPlotData);
+  const plotData = JSON.parse(localStorage.getItem("bmiData")) || [];
   const configuration = {
     type: "line",
     data: {
